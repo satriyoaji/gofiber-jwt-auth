1ade222163513b1b49b9f3157024b07a7f192619
satriyoaji <satriyoaji27@gmail.com>
2021-08-28 19:17:06 +0700

    fix some changes env

--- database/connection.go
+++ database/connection.go
@@ -11,7 +11,7 @@ var DB *gorm.DB
 
 func Connect()  {
 
-	connection, err := gorm.Open(mysql.Open(helper.GoDotEnvVariable("DB_USERNAME") + ":" + helper.GoDotEnvVariable("DB_PASSWORD") + "@tcp(localhost:" + helper.GoDotEnvVariable("DB_PORT") + ")/" + helper.GoDotEnvVariable("DB_NAME") + "?parseTime=True&loc=Local"), &gorm.Config{})
+	connection, err := gorm.Open(mysql.Open(helper.GoDotEnvVariable("DB_USERNAME") + ":" + helper.GoDotEnvVariable("DB_PASSWORD") + "@tcp("+helper.GoDotEnvVariable("DB_HOST")+":" + helper.GoDotEnvVariable("DB_PORT") + ")/" + helper.GoDotEnvVariable("DB_NAME") + "?parseTime=True&loc=Local"), &gorm.Config{})
 	helper.OutputPanicError(err)
 
 	DB = connection
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"gofiber-jwt-auth/database"
-	"gofiber-jwt-auth/helper"
-	"gofiber-jwt-auth/routes"
+	"github.com/satriyoaji/gofiber-jwt-auth/database"
+	"github.com/satriyoaji/gofiber-jwt-auth/helper"
+	"github.com/satriyoaji/gofiber-jwt-auth/routes"
 )
 
 func main() {
